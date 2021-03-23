8d71e59d0e01c05e0762e18d50c5255aeda5d1bf
fatih kgokmen <fatih.kgokmen@gmail.com>
2021-03-23 00:29:41 -0400

    bootsrap implemented

--- autodealer/settings.py
+++ autodealer/settings.py
@@ -120,5 +120,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'cardealer/static'),
+    os.path.join(BASE_DIR, 'autodealer/static'),
 ]
