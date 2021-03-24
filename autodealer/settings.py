3ae61843f554f2f77911075991e86acafec8d559
fatih kgokmen <fatih.kgokmen@gmail.com>
2021-03-24 01:18:38 -0400

    team database model implemented

--- autodealer/settings.py
+++ autodealer/settings.py
@@ -125,3 +125,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'autodealer/static'),
 ]
+
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
--- pages/models.py
+++ pages/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 
-class Teams(models.Model):
+class Team(models.Model):
 	first_name= models.CharField(max_length=255)
 	last_name= models.CharField(max_length=255)
 	designation= models.CharField(max_length=255)
@@ -11,6 +11,9 @@ class Teams(models.Model):
 	facebook_link = models.URLField(max_length=100)
 	twitter_link = models.URLField(max_length=100)
 	google_plus_link = models.URLField(max_length=100)
-	created_date = models.DataTimeField(auto_now_add=True)
+	created_date = models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		return self.first_name
 
 
