1b13a86587828470843aee4c02023f7e3538122c
fatih kgokmen <fatih.kgokmen@gmail.com>
2021-03-24 11:52:09 -0400

    search and filter implemented on admin panel

--- pages/admin.py
+++ pages/admin.py
@@ -8,8 +8,12 @@ from django.utils.html import format_html
 
 class TeamAdmin(admin.ModelAdmin):
 	def thumbnail(self,object):
-		return format_html('<img  src="{}" width="40" />'.format(object.photo.url))
+		return format_html('<img  src="{}" width="40" style="border-radius: 15px" />'.format(object.photo.url))
+
+	thumbnail.short_description = 'Image'
 	list_display = ('id' ,'thumbnail' ,'first_name' , 'designation' , 'created_date')
 	list_display_links = ('id','first_name',)
+	search_fields=('first_name' , 'last_name', 'designation')
+	list_filter = ('designation',)
 
 admin.site.register(Team, TeamAdmin)
