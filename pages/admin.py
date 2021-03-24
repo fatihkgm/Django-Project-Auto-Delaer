d889c79b878ad4ee9c73af1355b8ff966d3cf9e4
fatih kgokmen <fatih.kgokmen@gmail.com>
2021-03-24 11:19:14 -0400

    team admin panel updated with img

--- pages/admin.py
+++ pages/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
 from .models import Team
+from django.utils.html import format_html
 
 # Register your models here.
 
 
-admin.site.register(Team)
+
+class TeamAdmin(admin.ModelAdmin):
+	def thumbnail(self,object):
+		return format_html('<img  src="{}" width="40" />'.format(object.photo.url))
+	list_display = ('id' ,'thumbnail' ,'first_name' , 'designation' , 'created_date')
+	list_display_links = ('id','first_name',)
+
+admin.site.register(Team, TeamAdmin)
--- pages/views.py
+++ pages/views.py
@@ -6,12 +6,16 @@ def home (request):
 	teams = Team.objects.all()
 	data = {
 		'teams': teams,
-	}
-    return render(request, 'pages/home.html',data)
+	} 
+	return render(request, 'pages/home.html',data)
 
 
 def about (request):
-	return render(request, 'pages/about.html')
+	teams = Team.objects.all()
+	data = {
+		'teams' : teams,
+	}
+	return render(request, 'pages/about.html',data)
 
 def services (request):
 	return render(request, 'pages/services.html')
