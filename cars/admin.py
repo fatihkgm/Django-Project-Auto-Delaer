513e4da898b72473eb23c0db51c0203a15d796c4
fatih kgokmen <fatih.kgokmen@gmail.com>
2021-03-24 19:09:06 -0400

    car admin panel styled and implemented functional

--- cars/admin.py
+++ cars/admin.py
@@ -1,6 +1,21 @@
 from django.contrib import admin
 from .models import Car
+from django.utils.html import format_html
 
 # Register your models here.
 
-admin.site.register(Car)
+
+class CarAdmin(admin.ModelAdmin):
+	def thumbnail(self,object):
+		return format_html('<img  src="{}" width="40" style="border-radius: 15px" />'.format(object.car_photo.url))
+	thumbnail.short_description = ' Car Image'
+
+
+
+	list_display = ('id','thumbnail','car_title', 'condition' , 'model','year', 'body_style','is_featured')
+	list_display_links = ('id','thumbnail','car_title')
+	list_editable = ('is_featured',)
+	search_fields=('id','car_title', 'condition' , 'model','year', 'body_style')
+	list_filter = ('city',)
+admin.site.register(Car,CarAdmin)
+
--- cars/models.py
+++ cars/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from datetime import datetime
+from ckeditor.fields import RichTextField
+from multiselectfield import MultiSelectField
 
 # Create your models here.
 
@@ -97,19 +99,19 @@ class Car(models.Model):
 	year = models.IntegerField(('year'),choices= year_choice)
 	condition = models.CharField(max_length = 100)
 	price = models.IntegerField()
-	description = models.TextField(max_length = 500)
+	description = RichTextField()
 	car_photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
 	car_photo_1 = models.ImageField(upload_to='photos/%Y/%m/%d/' , blank = True)
 	car_photo_2 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank = True)
 	car_photo_3 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank = True)
 	car_photo_4 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank = True)
-	features = models.CharField(choices=features_choices,max_length=100)
+	features = MultiSelectField(choices=features_choices,max_length=300)
 	body_style = models.CharField(max_length = 100)
 	engine = models.CharField(max_length = 100)
 	transmission = models.CharField(max_length = 100)
 	interior = models.CharField(max_length = 100)
 	miles = models.IntegerField()
-	doors = models.IntegerField(choices=door_choices,max_length=10)
+	doors = models.CharField(choices=door_choices,max_length=100)
 	passengers = models.IntegerField()
 	vin_no = models.CharField(max_length = 100)
 	millage = models.IntegerField()
@@ -117,6 +119,10 @@ class Car(models.Model):
 	no_of_owners = models.CharField(max_length = 100)
 	is_featured = models.BooleanField(default=False)
 	created_date = models.DateTimeField(default=datetime.now,blank=True)
+
+
+	def __str__(self):
+		return self.car_title
 
 
 
