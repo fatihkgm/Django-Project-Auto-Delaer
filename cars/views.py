e2cc937304736f2b412416c9d0bac4bd8ad5a835
fatih kgokmen <fatih.kgokmen@gmail.com>
2021-04-13 16:31:42 -0400

    single page for car created
    "{% url 'car_detail' car.id %}"

--- cars/views.py
+++ cars/views.py
@@ -5,3 +5,7 @@ from django.shortcuts import render
 
 def cars(request):
 	return render(request, 'cars/cars.html')
+
+
+def car_detail(request, id):
+	return render(request, 'cars/car_detail.html')
